0e7fa142dc285251d00fc231bc3080440ff2bb43
kipyegonk <kevinlangat008@gmail.com>
2026-03-20 14:54:43 +0300

    feat: add malaria, TB, measles, AWD diseases + DHIS2 + Swahili + supervisor targets

--- targets.js
+++ targets.js
@@ -1,2 +1,22 @@
 const { chpTargets, chaTargets, dsoTargets } = require('./app_targets');
-module.exports = [...chpTargets, ...chaTargets, ...dsoTargets];
+const {
+  malariaTargets,
+  tbTargets,
+  measlesTargets,
+  awdTargets,
+  supervisorTargets,
+} = require('./app_targets/disease_targets');
+
+module.exports = [
+  // Existing cholera targets
+  ...chpTargets,
+  ...chaTargets,
+  ...dsoTargets,
+  // New disease targets (visible to all roles)
+  ...malariaTargets,
+  ...tbTargets,
+  ...measlesTargets,
+  ...awdTargets,
+  // Richer supervisor KPIs (CHA / DSO)
+  ...supervisorTargets,
+];
